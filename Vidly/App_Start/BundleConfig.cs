8a943d344d5a1950ff3383d542407a48446a8c1d
Antonio Carlos Pacheco Rui Dias <acprd_carlinhos@hotmail.com>
2018-02-12 21:39:03 -0200

    bootstrap4

--- Vidly/App_Start/BundleConfig.cs
+++ Vidly/App_Start/BundleConfig.cs
@@ -20,11 +20,12 @@ namespace Vidly
 						"~/Scripts/modernizr-*"));
 
 			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+					  "~/Scripts/umd/popper.js",
 					  "~/Scripts/bootstrap.js",
 					  "~/Scripts/respond.js"));
 
 			bundles.Add(new StyleBundle("~/Content/css").Include(
-					  "~/Content/bootstrap.css",
+					  "~/Content/bootswatch/superhero/bootstrap.css",
 					  "~/Content/site.css"));
 		}
 	}
